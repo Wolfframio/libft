47beb37c233ff53103be5f23765b6daac18fa97c
Wolfframio <116112114+Wolfframio@users.noreply.github.com>
2023-04-25 19:59:25 +0200

    mapi iteri libft Makefile home

--- libft.h
+++ libft.h
@@ -46,10 +46,12 @@ int		ft_isprint(int c);
 
 /*	STRING	*/
 
+void	ft_striteri(char *s, void (*f)(unsigned int, char*));
 int		ft_toupper(int c);
 int		ft_tolower(int c);
 int		ft_strncmp(const char *s1, const char *s2, size_t n);
 int		ft_atoi(const char *str);
+char	*ft_itoa(int n);
 char	*ft_strdup(const char *str);
 char	*ft_strchr(const char *s, int c);
 char	*ft_strrchr(const char *s, int c);
@@ -57,6 +59,7 @@ char	*ft_strnstr(const char *haystack, const char *needle, size_t len);
 char	*ft_substr(char const *s, unsigned int start, size_t len);
 char	*ft_strjoin(char const *s1, char const *s2);
 char	*ft_strtrim(char const *s1, char const *set);
+char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 size_t	ft_strlen(const char *str);
 size_t	ft_strlcpy(char	*dest, const char *src, size_t dstsize);
 size_t	ft_strlcat(char *dst, const char *src, size_t size);
